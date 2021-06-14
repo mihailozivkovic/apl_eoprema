a1ee3d08166857fcc7113cc79d7656e4e1159b9a
Mihailo Zivkovic <mihailo.zivkovic96@gmail.com>
2021-06-14 23:18:42 +0200

    Generate all remaining database entity models

--- entities/administrator.entity.ts
+++ entities/administrator.entity.ts
@@ -1,13 +1,27 @@
-import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity()
+@Index("uq_administrator_username", ["username"], { unique: true })
+@Entity("administrator")
 export class Administrator {
-    @PrimaryGeneratedColumn({name: 'administrator_id', type:'int', unsigned:true})
-    administratorId:number;
+  @PrimaryGeneratedColumn({
+    type: "int",
+    name: "administrator_id",
+    unsigned: true,
+  })
+  administratorId: number;
 
-    @Column({type:'varchar',length:'32', unique:true})
-    username:string;
+  @Column("varchar", {
+    name: "username",
+    unique: true,
+    length: 32,
+    default: () => "'0'",
+  })
+  username: string;
 
-    @Column({name:'password_hash', type:'varchar',length:'32', unique:true})
-    passwordHash:string;
+  @Column("varchar", {
+    name: "password_hash",
+    length: 128,
+    default: () => "'0'",
+  })
+  passwordHash: string;
 }
